b51d32ebaad7b9334a457764d442c7d5ac422824
wyben daal <wybendaal123@example.com>
2025-10-14 08:50:18 +0800

    Initial commit

---
+++ src/main/java/com/appdev/merchpoint/daalg6/Daalg6Application.java
@@ -0,0 +1,13 @@
+package com.appdev.merchpoint.daalg6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Daalg6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Daalg6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/appdev/merchpoint/daalg6/Daalg6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appdev.merchpoint.daalg6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Daalg6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
